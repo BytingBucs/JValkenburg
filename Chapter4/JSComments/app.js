3a54f262cb4818ba8da44d597441f1d620536374
jvalkenburg <jdvalkenburg@bucs.fsw.edu>
2016-02-17 21:53:47 -0500

    added enter key event handler

--- Chapter4/JSComments/app.js
+++ Chapter4/JSComments/app.js
@@ -7,6 +7,17 @@ var main = function(){
 		if ($(".comment-input input").val() !== ""){
 			$new_comment = $("<p>").text($(".comment-input input").val());
 			$(".comments").append($new_comment);
+			$(".comment-input input").val("");
+		}
+	});
+	
+	$(".comment-input input").on("keypress", function(event){
+		if (event.keyCode === 13){
+			if($(".comment-input input").val() !== ""){
+				var $new_comment = $("<p>").text($(".comment-input input").val());
+				$(".comments").append($new_comment);
+				$(".comment-input input").val("");
+			}
 		}
 	});
 };
