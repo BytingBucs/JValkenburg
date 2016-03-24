8b0857ca1ae75c763c7e4351e54dc88a657043fd
jvalkenburg <jdvalkenburg@bucs.fsw.edu>
2016-03-23 20:25:20 -0400

    updating app.js to match end of chapter 4

--- Chapter5/Amazeriffic/javascripts/app.js
+++ Chapter5/Amazeriffic/javascripts/app.js
@@ -5,32 +5,15 @@ var main = function (toDoObjects){
 		return toDo.description;
 	});
 	
-	var makeTabActive = function (tabNumber)
-	{
-		//construct the selector from the tabNumber
-		var tabSelector = ".tabs a:nth-child(" + tabNumber + ") span";
+	$(".tabs a span").toArray().forEach(function(element){
+		var $element = $(element);
+		
+		$element.on("click", function(){
 			$(".tabs span").removeClass("active");
-			$(tabSelector).addClass("active");
+			$(element).addClass("active");
 			$("main .content").empty();
-	};	
-	$(".tabs a:nth-child(1)").on("click", function(){
-		makeTabActive(1);
-		return false;
-	});
-	
-	$(".tabs a:nth-child(2)").on("click", function(){
-		makeTabActive(2);
-		return false;
-	});
-	
-	$(".tabs a:nth-child(3)").on("click", function(){
-		makeTabActive(3);
-		return false;
-	});
-	
-	$(document).ready(function(){
-		$.getJSON("todos.json", function(){
-			main(toDoObjects);
+			return false;
 		});
+	
 	});
 }
