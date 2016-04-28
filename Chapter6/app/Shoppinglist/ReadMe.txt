7e9e4c7bb82722585db0e34fd412ae7ca977f1e4
jvalkenburg <jdvalkenburg@bucs.fsw.edu>
2016-04-28 00:46:04 -0400

    uploading shopping list contribution assignment

---
+++ Chapter6/app/Shoppinglist/ReadMe.txt
@@ -0,0 +1,25 @@
+Contributions
+-Some work on choosing fonts with the css. Changed css to match scheme picked out by group
+in class.
+
+-Added a "Stores" tab, JSON tags and changed code to display the items sorted by the stores
+they were located in.
+
+To dos
+-Complete the functionality of the application from chapter 9. Code currently throws an error
+when trying to add an item to the list via the "+" button on the add tab.
+
+-Add the capability to remove items.
+
+
+-Add user controls
+
+
+Summary
+Through this course I learned alot about the functionality that JavaScript can add to a 
+web application. The combination of objects through JSON, styling through CSS, structure through
+HTML, and functionality through JavaScript gives an app what it needs to function as if it
+were a program directly installed on the user's computer.  I would like to get a better understanding
+of some of the tools used like Vagrant, git.bash, Express and Mongo. These are good tools to test and run
+an app before publishing, but my lack of knowledge of the commands, and how to handle error messages, made this
+project difficult.
---
+++ Chapter6/app/Shoppinglist/client/javascripts/test.js
@@ -0,0 +1,190 @@
+var main = function(toDoObjects){
+	var toDos, 
+		tabs;
+	toDos= toDoObjects.map(function(toDo){
+			return toDo.description;
+		});
+	tabs = [];
+		
+	tabs.push({
+		//name of tab
+		"name":"Newest ",
+	
+		//functionality
+		"content": function(){
+			$.get("todos.json", function(toDoObjects){
+				var $content;
+			
+				$content = $("<ul>");
+				for(i = toDos.length-1; i>=0; i--){
+					$content.append($("<li>").text(toDos[i]));
+				}
+				$("main .content").append($content);
+			});
+		}
+	});
+	
+	tabs.push({
+		"name":"Oldest ",
+		"content": function(){
+			$.get("todos.json", function(toDoObjects){
+				var $content;
+				$content = $("<ul>");
+				
+				toDos.forEach(function(todo){
+				$content.append($("<li>").text(todo));
+				});
+				$("main .content").append($content);
+			});
+		}
+	});
+	
+	tabs.push({
+		"name":"Tags ",
+		"content": function(){
+			$.get("todos.json", function(toDoObjects){
+				var tags = [];
+						
+					toDoObjects.forEach(function (toDo){
+						toDo.tags.forEach(function (tag){
+							if(tags.indexOf(tag) === -1) {
+								tags.push(tag);
+							}
+						});
+					});
+						
+					var tagObjects = tags.map(function (tag) {
+						var toDosWithTag = [];
+						toDoObjects.forEach(function(toDo){
+							if (toDo.tags.indexOf(tag) !== -1){
+								toDosWithTag.push(toDo.description);
+							}
+						});
+						return {"name": tag, "toDos": toDosWithTag };
+					});				
+					tagObjects.forEach(function(tag){
+						var $tagName = $("<h3>").text(tag.name),
+							$content = $("<ul>");
+							
+						tag.toDos.forEach(function(description){
+							var $li = $("<li>").text(description);
+							$content.append($li);
+						});
+							
+						$("main .content").append($tagName);
+						$("main .content").append($content);
+					});
+			});
+		}
+	});
+		tabs.push({
+		"name":"Stores ",
+		"content": function(){
+			$.get("todos.json", function(toDoObjects){
+				var stores = [];
+						
+					toDoObjects.forEach(function (toDo){
+						toDo.stores.forEach(function (store){
+							if(stores.indexOf(store) === -1) {
+								stores.push(store);
+							}
+						});
+					});
+						
+					var storeObjects = stores.map(function (store) {
+						var toDosWithstore = [];
+						toDoObjects.forEach(function(toDo){
+							if (toDo.stores.indexOf(store) !== -1){
+								toDosWithstore.push(toDo.description);
+							}
+						});
+						return {"name": store, "toDos": toDosWithstore };
+					});				
+					storeObjects.forEach(function(store){
+						var $storeName = $("<h3>").text(store.name),
+							$content = $("<ul>");
+							
+						store.toDos.forEach(function(description){
+							var $li = $("<li>").text(description);
+							$content.append($li);
+						});
+							
+						$("main .content").append($storeName);
+						$("main .content").append($content);
+					});
+			});
+		}
+	});
+		tabs.push({
+		"name":"Add",
+		"content":function(){
+			$.get("todos.json", function(toDoObjects){
+			var $input = $("<input>").addClass("description"),
+					$inputLabel = $("<p>").text("Description: "),
+					$tagInput = $("<input>").addClass("tags"),
+					$tagLabel = $("<p>").text("Tags: "),
+					$storeInput = $("<input>").addClass("stores"),
+					$storeLabel= $("<p>").text("Stores: "),
+					$button = $("<button>").text("+");
+
+                $button.on("click", function () {
+                    var description = $input.val(),
+						tags = $tagInput.val().split(","),
+						stores = $storeInput.val().split(","),
+						
+						newToDo = {"description":description, "tags":tags, "stores":stores};
+					
+					
+					$.post("todos", newToDo, function(result){
+						$input.val("");
+						$tagInput.val("");
+						
+						$(".tabs a:first span").trigger("click");
+					});
+					
+				});
+				$content = $("<div>").append($inputLabel)
+										.append($input)
+										.append($tagLabel)
+										.append($tagInput)
+										.append($storeLabel)
+										.append($storeInput)
+										.append($button);
+				$("main .content").append($content);
+				return $content;
+			});
+		}
+	});
+	
+	tabs.forEach(function (tab){
+		var $aElement = $("<a>").attr("href",""),
+			$spanElement = $("<span>").text(tab.name);
+			
+		$aElement.append($spanElement);
+		
+		$spanElement.on("click", function(){
+			var $content;
+			
+			$(".tabs a span").removeClass("active");
+			$spanElement.addClass("active");
+			$("main .content").empty();
+			
+			$content = tab.content();
+			
+			$("main .content").append($content);
+			return false;
+		});
+		$("main .tabs").append($aElement);
+	});
+	
+/*	console.log("hello")
+	console.log(toDos);
+	console.log(tabs);*/
+
+};
+
+$(document).ready(function(){
+	$.getJSON("todos.json", function(toDoObjects){
+		main(toDoObjects);
+	});
+});
